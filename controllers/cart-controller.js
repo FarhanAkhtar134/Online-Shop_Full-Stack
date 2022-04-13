d77214769fc5a9b3347545ee6b710447bbc6de69
Farhan Akhtar <farhandx1@hotmail.com>
2022-04-13 14:11:05 +0800

    Added Cart Model, controller and route for adding items to cart

---
+++ controllers/cart-controller.js
@@ -0,0 +1,31 @@
+const Product = require('../models/product-model'); 
+
+async function addCartItem(req, res, next) {
+    let product; 
+    try {
+        
+         product = await Product.findById(req.body.productId); 
+    } catch (error) {
+        next(error); 
+        return; 
+        
+    }
+
+
+    const cart = res.locals.cart;
+
+    cart.addCartItem(product); 
+    req.session.cart = cart; 
+
+    res.status(201).json({
+        message:'Cart updated!',
+        newTotalItems: cart.totalQuantity,
+
+    }); 
+
+    
+}; 
+
+module.exports = {
+    addCartItem: addCartItem, 
+}
