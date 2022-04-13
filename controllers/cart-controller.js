0ff80e457c10ff3bc5ef45ef10d4e4b5a9127c62
Farhan Akhtar <farhandx1@hotmail.com>
2022-04-13 17:37:41 +0800

    Added cart view and style for cart view

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -1,5 +1,11 @@
 const Product = require('../models/product-model'); 
 
+function getCart(req, res) {
+
+
+    res.render('customer/cart/cart'); 
+}
+
 async function addCartItem(req, res, next) {
     let product; 
     try {
@@ -29,4 +35,5 @@ async function addCartItem(req, res, next) {
 
 module.exports = {
     addCartItem: addCartItem, 
+    getCart: getCart, 
 }
