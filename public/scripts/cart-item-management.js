b3f7c0a8cee4317297faffe8a03c6419f0636b55
Farhan Akhtar <farhandx1@hotmail.com>
2022-04-15 20:10:01 +0800

    bug fixes

--- public/scripts/cart-item-management.js
+++ public/scripts/cart-item-management.js
@@ -2,7 +2,7 @@ const cartItemUpdateFormElements = document.querySelectorAll(
   ".cart-item-management"
 );
 
-const cartBadge = document.querySelector(".nav-items .badge");
+const cartBadgeElements = document.querySelectorAll(".nav-items .badge");
 const cartTotalPriceElement = document.getElementById("cart-total-price");
 
 async function updateCartItem(event) {
@@ -55,8 +55,11 @@ async function updateCartItem(event) {
 
   cartTotalPriceElement.textContent = responseData.updatedCartData.newTotalPrice.toFixed(2);
 
-  
-  cartBadge.textContent = responseData.updatedCartData.newTotalQuantity;
+  for(const cartBadge of cartBadgeElements) {
+
+    cartBadge.textContent = responseData.updatedCartData.newTotalQuantity;
+  }
+
 }
 
 for (const formElement of cartItemUpdateFormElements) {
--- public/scripts/cart-management.js
+++ public/scripts/cart-management.js
@@ -2,7 +2,7 @@ const addToCartButtonElement = document.querySelector(
   "#product-details button"
 );
 
-const cartBadgeElement = document.querySelector(".nav-items .badge");
+const cartBadgeElements = document.querySelectorAll(".nav-items .badge");
 
 async function addToCart() {
     console.log('button clicked')
@@ -35,7 +35,11 @@ async function addToCart() {
 
   const newTotalQuantity = responseData.newTotalItems; 
 
-  cartBadgeElement.textContent = newTotalQuantity; 
+  for(const cartBadgeElement of cartBadgeElements ) {
+
+    cartBadgeElement.textContent = newTotalQuantity; 
+  }
+
 }
 
 addToCartButtonElement.addEventListener("click", addToCart);
